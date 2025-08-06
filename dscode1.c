eed13dd99e8a9f20e3492183595c7e331a956f1a
deekshi-28 <deekshiush@gmail.com>
2025-08-07 05:20:54 +0530

    Add files via upload

---
+++ dscode1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int A[10][10], B[10][10], C[10][10], m, n, p, i, j, k;
+    printf("Enter rows and cols of Matrix A: ");
+    scanf("%d%d", &m, &n);
+    printf("Enter cols of Matrix B: ");
+    scanf("%d", &p);
+
+    printf("Enter elements of Matrix A:\n");
+    for (i = 0; i < m; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &A[i][j]);
+
+    printf("Enter elements of Matrix B:\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < p; j++)
+            scanf("%d", &B[i][j]);
+
+    for (i = 0; i < m; i++)
+        for (j = 0; j < p; j++) {
+            C[i][j] = 0;
+            for (k = 0; k < n; k++)
+                C[i][j] += A[i][k] * B[k][j];
+        }
+
+    printf("Resultant Matrix C:\n");
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < p; j++)
+            printf("%d ", C[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
